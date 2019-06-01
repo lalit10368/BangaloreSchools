596091af13ccfa84e7f2fe43d7f5e9cbfee4036f
Lalit Kumar <Lalit.Kumar@paysafe.com>
2019-06-01 17:15:20 +0530

    [util] : added CsvParser

---
+++ util/CsvParser.js
@@ -0,0 +1,38 @@
+var fs = require('fs');
+var readline = require('readline');
+var _ = require('lodash');
+var Promise = require("bluebird");
+
+
+module.exports = function(fileName, delimiter) {
+
+    return new Promise(function(resolve) {
+
+        var jsonData = [];
+
+        var rl = readline.createInterface({
+            input: fs.createReadStream(fileName),
+            crlfDelay: Infinity
+        });
+
+
+        var keys;
+        rl.on('line', function(line) {
+            if(keys == undefined) {
+                keys = line.split(delimiter).map(_.trim);
+            } else {
+                var values = line.split(delimiter).map(_.trim);
+                if(values.length == keys.length) {
+                    var object = _.zipObject(keys, values);
+                    console.log(object);
+                    jsonData.push(object);
+                }
+            }
+
+        });
+
+        rl.on('close', function() {
+            resolve(jsonData);
+        });
+    });
+};
